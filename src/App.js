0ad20b8af8ea74a7997b87fa72d924a42947009b
Raniyaaaa <raniya182002@gmail.com>
2024-11-11 22:05:37 +0530

    updating profile details

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./Pages/Login";
 import Products from "./Pages/Products";
 import AboutUs from "./Pages/AboutUs";
-import MainNavigation from "./MainNavigation/MainNavigation";
 import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
 
 function App() {
 return(
   <Router>
-    <MainNavigation/>
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path="/products" element={<Products/>}/>
       <Route path='/aboutus' element={<AboutUs/>}/>
       <Route path='/home' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}></Route>
     </Routes>
   </Router>
 )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
+import { Navbar, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Home=()=>{
-    return(
-        <h3 style={{textAlign:'center',padding:'2rem'}}>
-            Welcome to Expense Tracker!!!
-        </h3>
-    )
-
+const Home = () => {
+  return (
+    <>
+      <Navbar style={{ border: '1px solid black', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '4rem', padding: '0 1rem' }}>
+        <Navbar.Brand>Welcome to Expense Tracker!!!</Navbar.Brand>
+        
+        <Alert variant="danger" style={{ display: 'flex', alignItems: 'center', margin: 0, padding: '0.5rem 1rem' }}>
+          <span style={{ marginRight: '0.5rem' }}>Your profile is incomplete.</span>
+          <Link to='/profile' style={{ color: '#0d6efd', fontWeight: 'bold' }}>Complete now</Link>
+        </Alert>
+      </Navbar>
+    </>
+  );
 }
+
 export default Home;
