2c5bbbfba4b3a3d59d5cd4fb22b7105f152840ef
Raniyaaaa <raniya182002@gmail.com>
2024-11-11 15:01:53 +0530

    Login Screen

--- src/MainNavigation/MainNavigation.js
+++ src/MainNavigation/MainNavigation.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Container, Navbar, NavLink } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const MainNavigation=()=>{
     return(
         <Navbar bg="white">
             <Container>
-                <NavLink style={{color:'powderblue',fontSize:'2rem' }}>MyWebLink</NavLink>
-                <NavLink to='/home' style={{color: 'grey'}}>Home</NavLink>
-                <NavLink to='/products' style={{color: 'grey'}}>Products</NavLink>
-                <NavLink to='/aboutus' style={{color: 'grey'}}>About Us</NavLink>
+            <Nav className="me-auto">
+                <NavLink to='/' style={{color:'powderblue',fontSize:'1.5rem',marginRight: '1rem',textDecoration: 'none'}}>MyWebLink</NavLink>
+                <NavLink to='/home' style={{color: 'grey', marginRight: '1rem',textDecoration: 'none' }}>Home</NavLink>
+                <NavLink to='/products' style={{color: 'grey', marginRight: '1rem',textDecoration: 'none' }}>Products</NavLink>
+                <NavLink to='/aboutus' style={{color: 'grey', marginRight: '1rem',textDecoration: 'none' }}>About Us</NavLink>
+            </Nav>    
             </Container>
         </Navbar>
     )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,10 @@
-import react from "react"
 
 const Home=()=>{
+    return(
+        <h3 style={{textAlign:'center',padding:'2rem'}}>
+            Welcome to Expense Tracker!!!
+        </h3>
+    )
 
 }
 export default Home;
