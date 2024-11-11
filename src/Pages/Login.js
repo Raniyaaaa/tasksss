4710d220b890d246b81e301ee91e41676f365f02
Raniyaaaa <raniya182002@gmail.com>
2024-11-11 15:01:53 +0530

    Login Screen

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,22 +1,38 @@
-import react, { useRef, useState } from "react"
-import { Button } from "react-bootstrap";
+import { useRef, useState } from "react"
+import { Button, FloatingLabel, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login=()=>{
     const [isLogin,setIsLogin]=useState(false)
     const emailInputRef=useRef();
     const passwordInputRef=useRef();
     const confirmPasswordInputRef=useRef();
+    const navigate=useNavigate();
+    
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false)
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible((prevState) => !prevState);
+    }
+
+    const toggleConfirmPasswordVisibility = () => {
+        setConfirmPasswordVisible((prevState) => !prevState);
+    };
 
     const submitHandler=(event)=>{
         event.preventDefault();
         const enteredEmail=emailInputRef.current.value;
         const enteredPassword=passwordInputRef.current.value;
-        const enteredConfirmPassword=confirmPasswordInputRef.current.value;
-        if (passwordInputRef.current.value !== confirmPasswordInputRef.current.value) {
+        const enteredConfirmPassword = confirmPasswordInputRef.current ? confirmPasswordInputRef.current.value : '';
+
+        if (!isLogin && enteredPassword !== enteredConfirmPassword) {
             alert("Passwords do not match!");
             return;
         }
-        
+
         let url;
         if(isLogin){
             url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC465zamb1d08zID26EyUI0YPtMg_TO9qw'
@@ -29,7 +45,6 @@ const Login=()=>{
             body:JSON.stringify({
                 email:enteredEmail,
                 password:enteredPassword,
-                confirmPassword:enteredConfirmPassword,
                 returnSecureToken:true
             }),
             headers:{
@@ -51,55 +66,119 @@ const Login=()=>{
         })
         .then((data)=>{
             console.log("User has successfully signed up")
+            emailInputRef.current.value = '';
+            passwordInputRef.current.value = '';
+            if (confirmPasswordInputRef.current) {
+                confirmPasswordInputRef.current.value = '';
+            }
+            navigate("/home")
         })
         .catch((err)=>{
             alert(err)
         })
     }
+
+    const toggleAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
+
     return(
         <>
-        <div style={{paddingTop:'10rem',paddingRight:'5rem',paddingLeft:'40rem'}}>
-            <section>
+        <div style={{ paddingTop: '2rem',paddingLeft:'20rem',paddingBottom:'5rem', display: 'flex', alignItems: 'center' ,flexDirection: 'column'}}>
+            <section style={{
+                border:'1px solid #ccc',
+                borderRadius:'2px',
+                padding:'2rem',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                width:'350px',
+                textAlign:'center'
+                }}>
                 <h1>{isLogin? 'Login':'SignUp'}</h1>
                 <form onSubmit={submitHandler}>
-                    <div>
-                        <label htmlFor="email">
-                            <input 
-                            type='email' 
-                            id='email' 
-                            ref={emailInputRef}
-                            placeholder="Email"
-                            required></input>
-                        </label>
+                    <div className="mt-4">
+                        <FloatingLabel label="Email" controlId="email">
+                            <input
+                                className="form-control"
+                                type="email"
+                                ref={emailInputRef}
+                                style={{ height: "35px", fontSize: "14px" }}
+                                required
+                            />
+                        </FloatingLabel>
                     </div>
-                    <div>
-                        <label htmlFor="Password">
-                            <input 
-                            type='Password' 
-                            id='Password' 
-                            ref={passwordInputRef}
-                            placeholder="Password"
-                            required></input>
-                        </label>
+                    <div className="mt-2">
+                        <FloatingLabel label="Password" controlId="password">
+                            <input
+                                type={passwordVisible ? "text" : "password"}
+                                ref={passwordInputRef}
+                                className="form-control"
+                                style={{ height: "35px", fontSize: "14px" }}
+                                required
+                                
+                            />
+                            <Button
+                                variant="link"
+                                style={{
+                                    position: 'absolute',
+                                    right: '10px',
+                                    top: '10px',
+                                    backgroundColor: 'transparent',
+                                    border: 'none',
+                                    }}
+                                onClick={togglePasswordVisibility}
+                            >
+                                {passwordVisible ? <FaEyeSlash/> : <FaEye />}
+                            </Button>
+                        </FloatingLabel>
+                        
                     </div>
-                    <div>
-                        <label htmlFor="ConfirmPassword">
-                            <input 
-                            type='Password' 
-                            id='ConfirmPassword' 
-                            ref={confirmPasswordInputRef}
-                            placeholder="Confirm Password"
-                            required></input>
-                        </label>
+                    {!isLogin && <div className="mt-2">
+                        <FloatingLabel label="Confirm Password" controlId="confirmPassword">
+                            <input
+                                type={confirmPasswordVisible ? "text" : "password"}
+                                ref={confirmPasswordInputRef}
+                                className="form-control"
+                                style={{ height: "35px", fontSize: "14px"}}
+                                required
+                            />
+                            <Button
+                                variant="link"
+                                style={{
+                                    position: 'absolute',
+                                    right: '10px',
+                                    top: '10px',
+                                    backgroundColor: 'transparent',
+                                    border: 'none',
+                                }}
+                                onClick={toggleConfirmPasswordVisibility}
+                            >
+                                {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                            </Button>
+                        </FloatingLabel>
+                    </div>}
+                    <div className="mt-4" style={{ display: "flex", flexDirection: "column" }}>
+                        <Button variant="primary" type="submit" style={{ borderRadius: "10rem" }}>
+                            {isLogin ? "Login" : "SignUp"}
+                        </Button>
                     </div>
-                    <div>
-                        <Button variant="primary" type="submit">{isLogin? 'Login':'SignUp'}</Button>
-                    </div>
+                    {isLogin &&<div className="mt-1">
+                        <Link>forgot password?</Link>
+                    </div>}
                 </form>
             </section>
-            <section>
-            <button type='button' >{isLogin? 'Create New Account': 'Have an Account? Login'}</button>
-            </section>
+            <div className="mt-3" style={{ width: "350px" }}>
+                <Alert
+                    variant="success"
+                    onClick={toggleAuthModeHandler}
+                    style={{
+                    cursor: "pointer",
+                    padding: "0.5rem 3rem",
+                    width: "100%",
+                    textAlign: "center",
+                    }}>
+                    {isLogin ? "Don't have an Account? Sign Up" : "Have an Account? Login"}
+                </Alert>
+            </div>
         </div>
         </>
     )
