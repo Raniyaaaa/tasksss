6f6cd494caadb13abf4a23dc83d2591646488e19
Raniyaaaa <raniya182002@gmail.com>
2024-11-12 10:40:37 +0530

    Edit User Details

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,17 +1,38 @@
 import { Navbar, Alert,Button } from "react-bootstrap";
 import { FaGithub, FaGlobe } from "react-icons/fa";
 import { Link,useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Profile = () => {
-    const fullNameRef = useRef();
-    const photoURLRef = useRef();
     const navigate = useNavigate();
+    const [percentage,setPercentage]=useState('64')
+    const [fullName,setFullName]=useState("")
+    const [photoURL,setPhotoURL]=useState("")
+
+    useEffect(()=>{
+      const idToken=localStorage.getItem('token');
+      if(idToken){
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyC465zamb1d08zID26EyUI0YPtMg_TO9qw',{
+          method:'POST',
+          headers:{ 'Content-Type': 'application/json'},
+          body:JSON.stringify({idToken})
+        })
+        .then(res=>res.json())
+        .then(data=>{
+          console.log(data)
+          if (data.users && data.users.length > 0) {
+            setFullName(data.users[0].displayName || '');
+            setPhotoURL(data.users[0].photoUrl || '');
+            setPercentage('100')
+          }
+        })
+        .catch(error => console.log('Error fetching data',error))
+      }
+    },[])
 
     const profileSubmitHandler=(event)=>{
         event.preventDefault();
-        const enteredFullName=fullNameRef.current.value;
-        const enteredPhotoURL=photoURLRef.current.value;
+
         const url='https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyC465zamb1d08zID26EyUI0YPtMg_TO9qw'
 
         fetch(url, {
@@ -19,8 +40,8 @@ const Profile = () => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               idToken: localStorage.getItem('token'),
-              displayName: enteredFullName,
-              photoUrl: enteredPhotoURL,
+              displayName: fullName,
+              photoUrl: photoURL,
               returnSecureToken: true,
             }),
           })
@@ -42,8 +63,8 @@ const Profile = () => {
         <Navbar.Brand>Winners never quite, Quitters never win.</Navbar.Brand>
         
         <Alert variant="danger" style={{ display: 'flex', alignItems: 'center', margin: 0, padding: '0.5rem 1rem' }}>
-          <span style={{ marginRight: '0.5rem' }}>Your profile is <strong>64%</strong> completed. A complete Profile has higher chances of landing a job.</span>
-          <Link style={{ color: '#0d6efd', fontWeight: 'bold' }}>Complete now</Link>
+          <span style={{ marginRight: '0.5rem' }}>Your profile is <strong>{percentage}%</strong> completed. A complete Profile has higher chances of landing a job.</span>
+          {percentage!=='100'&&(<Link style={{ color: '#0d6efd', fontWeight: 'bold' }}>Complete now</Link>)}
         </Alert>
       </Navbar>
       <div>
@@ -54,10 +75,20 @@ const Profile = () => {
             </div>
             <form onSubmit={profileSubmitHandler} style={{marginTop:'1rem',display:'flex',justifyContent:'space-between'}}>
                 <label style={{marginRight:'1rem'}}><FaGithub/> Full Name 
-                    <input type='text' required ref={fullNameRef}></input>
+                    <input 
+                    type='text' 
+                    required 
+                    value={fullName}
+                    onChange={(e)=> setFullName(e.target.value)}
+                    ></input>
                 </label>
                 <label><FaGlobe/> Profile URL 
-                    <input type='text' required ref={photoURLRef}></input>
+                    <input 
+                    type='text' 
+                    required 
+                    value={photoURL}
+                    onChange={(e)=> setPhotoURL(e.target.value)}
+                    ></input>
                 </label>
                 <div style={{marginTop:'1rem',flexDirection:'column'}}>
                     <Button variant="danger" type="submit">Update</Button>
