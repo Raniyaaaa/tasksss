d4b4ffc25ee093944581de32cbb7b7fb5fc5484c
Raniyaaaa <raniya182002@gmail.com>
2024-11-12 13:10:42 +0530

    Logout User

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,19 @@ import { Navbar, Alert,Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  const navigate=useNavigate();
+
+  const logoutHandler=()=>{
+    localStorage.removeItem("token");
+    navigate('/')
+  }
   
   return (
     <>
       <Navbar style={{ border: '1px solid black', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '4rem', padding: '0 1rem' }}>
         <Navbar.Brand>Welcome to Expense Tracker!!!</Navbar.Brand>
-        
+        <Button variant="outline-danger" onClick={logoutHandler}>LOG OUT</Button>
         <Alert variant="danger" style={{ display: 'flex', alignItems: 'center', margin: 0, padding: '0.5rem 1rem' }}>
           <span style={{ marginRight: '0.5rem' }}>Your profile is incomplete.</span>
           <Link to='/profile' style={{ color: '#0d6efd', fontWeight: 'bold' }}>Complete now</Link>
